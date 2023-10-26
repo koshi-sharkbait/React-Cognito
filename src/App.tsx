73abd4074983d6df69be5eedd4dbfbc23d5df1ea
koshi-sharkbait <koshi.sharkbait@gmail.com>
2023-10-26 15:55:13 +0900

    Cognito実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,25 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import { Amplify } from 'aws-amplify';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { useEffect, useState } from 'react';
-import Amplify, { Auth, Hub } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth";
 
 Amplify.configure({
-  Auth: {
-    region: 'ap-northeast-1',
-    userPoolId: 'ap-northeast-1_XXXXXXXXXX',
-    userPoolWebClientId: 'XXXXXXXXXXXXXX',
-    oauth: {
-      domain: 'XXXXXXXXXXXXXX.auth.ap-northeast-1.amazoncognito.com',
-      scope: ['openid'],
-      redirectSignIn: 'https://localhost:3000/',
-      redirectSignOut: 'https://localhost:3000/',
-      responseType: 'code'
-    }
-  }
-})
+  aws_project_region: process.env.REACT_APP_AWS_PROJECT_REGION,
+  aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id:  process.env.REACT_APP_AWS_USER_POOLS_CLIENT_ID,
+});
 
-
-const Example = () => {
-  const [user, setUser] = useState<any | null>(null);
-
-  useEffect(() => {
-    Hub.listen('auth', ({ payload: { event, data } }) => {
-      switch (event) {
-        case 'signIn':
-        case 'cognitoHostedUI':
-          getUser().then(userData => setUser(userData));
-          break;
-        case 'signOut':
-          setUser(null);
-          break;
-        case 'signIn_failure':
-        case 'cognitoHostedUI_failure':
-          console.log('Sign in failure', data);
-          break;
-      }
-    });
-
-    getUser().then(userData => setUser(userData));
-  }, []);
-
-  const getUser = async () => {
-    try {
-      const userData = await Auth.currentAuthenticatedUser();
-      // デバッグ用
-      Auth.currentSession().then((data) => {
-        console.log(`token: ${data.getIdToken().getJwtToken()}`);
-      });
-      console.log(userData);
-      return userData;
-    } catch (e) {
-      return console.log('Not signed in');
-    }
-  }
-
-  return user ? (
-    <div>
-      <p>サインイン済み</p>
-      <p>ユーザー名: {user.username}</p>
-      <button onClick={() => Auth.signOut()}>Sign Out</button>
-    </div>
-  ) : (
-    <div>
-      <p>
-        サインインする
-      </p>
-      <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
-    </div>
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
-export default Example
